da46bdbfef964dbd83fb3761c1dfc0411763dde4
tzuyi <a0988426059@gmail.com>
2024-02-19 18:05:13 +0800

    chore: script command

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 const buildEslintCommand = filenames => {
-  return `cd website && next lint --fix --file ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`;
+  return `pnpm --filter website next lint --fix --file ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`;
 };
 
 module.exports = {
   'website/**/*.{js,jsx,ts,tsx}': [buildEslintCommand],
-  'mind-mapping/**/*.{js,ts}': ['cd .. && pnpm lint'],
+  'mind-mapping/**/*.{js,ts}': ['pnpm --filter -g lint'],
 };
