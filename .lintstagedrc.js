93e5a8742af67b0c7e3d003deb5a70cb925a6cb7
tzuyi <a0988426059@gmail.com>
2024-02-19 17:22:09 +0800

    fix: lint staged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,10 @@
 const path = require('path');
 
-const buildEslintCommand = filenames =>
-  `next lint --fix --file ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`;
+const buildEslintCommand = filenames => {
+  return `cd website && next lint --fix --file ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`;
+};
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  'website/**/*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  'mind-mapping/**/*.{js,ts}': ['cd .. && pnpm lint'],
 };
