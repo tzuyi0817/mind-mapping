8ceadffffa4674503495ced90bf468d6aa161faf
tzuyi <a0988426059@gmail.com>
2024-04-19 15:39:00 +0800

    docs: adjust eslint flat pattern config

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,40 @@
 const tsEslint = require('typescript-eslint');
-const eslintConfigPrettier = require('eslint-config-prettier');
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const configPrettier = require('eslint-config-prettier');
+const pluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 const pluginSecurity = require('eslint-plugin-security');
+const pluginNext = require('@next/eslint-plugin-next');
+const globals = require('globals');
+
+const nextConfig = {
+  files: ['website/**/*.{js,ts,tsx}'],
+  plugins: {
+    '@next/next': pluginNext,
+  },
+  rules: {
+    ...pluginNext.configs.recommended.rules,
+    ...pluginNext.configs['core-web-vitals'].rules,
+    // waiting for @next/eslint-plugin-next to support eslint flat pattern
+    '@next/next/no-duplicate-head': 'off',
+  },
+};
 
 module.exports = [
   ...tsEslint.configs.recommended,
-  eslintConfigPrettier,
-  eslintPluginPrettierRecommended,
+  configPrettier,
+  pluginPrettierRecommended,
   // 'plugin:sonarjs/recommended',
   pluginSecurity.configs.recommended,
+  nextConfig,
+  { files: ['**/*.js', '**/*./ts', '**/*./txs'] },
   {
-    ignores: ['node_modules', 'dist', 'pnpm-lock.yaml'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    ignores: ['node_modules/*', '**/dist/*', '**/pnpm-lock.yaml', 'website/.next/*', '**/*.d.ts'],
   },
 ];
